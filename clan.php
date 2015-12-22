d1671a9399c3a0510874a905e2147e87daf31a3f
Michael <michaeljin.lai@gmail.com>
2015-12-23 04:26:53 +1100

    Layout progress

---
+++ clan.php
@@ -0,0 +1,14 @@
+<?php 
+	require($_SERVER['DOCUMENT_ROOT']."/wartracker/database.php"); 
+	require($_SERVER['DOCUMENT_ROOT']."/wartracker/templatestart.php"); 
+?>
+
+        
+          Insert random shit
+
+<?php require($_SERVER['DOCUMENT_ROOT']."/wartracker/templatestop.php"); ?>
+
+
+
+
+
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-	require($_SERVER['DOCUMENT_ROOT']."/clashofclans/database.php"); 
+	require($_SERVER['DOCUMENT_ROOT']."/wartracker/database.php"); 
 ?>
 
 <!DOCTYPE html>
@@ -22,13 +22,34 @@
     <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/dashboard.css" rel="stylesheet">
+    <link href="css/fonts.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
   </head>
 
   <body>
 
-    <div class="col-sm-3 col-md-2 sidebar">
+    <div class="col-sm-3 col-md-2 sidebar" id="sidebar-open">
+      <ul class="nav nav-sidebar">
+        <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Reports</a></li>
+        <li><a href="#">Analytics</a></li>
+        <li><a href="#">Export</a></li>
+      </ul>
+      <ul class="nav nav-sidebar">
+        <li><a href="">Nav item</a></li>
+        <li><a href="">Nav item again</a></li>
+        <li><a href="">One more nav</a></li>
+        <li><a href="">Another nav item</a></li>
+        <li><a href="">More navigation</a></li>
+      </ul>
+      <ul class="nav nav-sidebar">
+        <li><a href="">Nav item again</a></li>
+        <li><a href="">One more nav</a></li>
+        <li><a href="">Another nav item</a></li>
+      </ul>
+    </div>
+
+    <div class="col-sm-3 col-md-2 sidebar hide" id="sidebar-close">
       <ul class="nav nav-sidebar">
         <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
         <li><a href="#">Reports</a></li>
@@ -249,10 +270,16 @@
 <script>
   $(document).ready(function() {
     $('#navigation-toggle').on("click", function(e) {
-      $('body').toggleClass('sidebar-open');
+      $('#sidebar-open').toggleClass('hide');
+      $('#sidebar-close').toggleClass('hide');
     });
   });
 </script>
+
+
+
+
+
 
 
 
