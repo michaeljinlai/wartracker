d53757444f823296349614ad168ba186db8fc062
Michael <michaeljin.lai@gmail.com>
2015-12-23 21:52:44 +1100

    Add font awesome and hamburger button

--- templatestart.php
+++ templatestart.php
@@ -20,6 +20,7 @@
     <!-- Custom styles for this template -->
     <link href="css/fonts.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
   </head>
 
   <body>
@@ -78,7 +79,9 @@
 	            <span class="icon-bar"></span>
 	            <span class="icon-bar"></span>
 	          </button>
-	          <a class="navbar-brand" href="#">Project name</a>
+	          <a class="btn btn-primary hamburger-btn">
+	          	<i class="fa fa-bars"></i>
+	          </a>
 	        </div>
 	        <div id="navbar" class="navbar-collapse collapse">
 	          <ul class="nav navbar-nav navbar-right">
